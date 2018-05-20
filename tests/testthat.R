51de836165f567fd022e07729728491a8391d9a8
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2018-05-20 11:35:38 +0200

    tests for codecov

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(hugo)
+
+test_check("hugo")
---
+++ tests/testthat/test_variable_response.R
@@ -0,0 +1,12 @@
+context("Check hugo_start_investigation() function")
+
+hugo_start_investigation("hugo_test")
+
+test_that("there is a .session_info", {
+  expect_true(".session_info" %in% list.files("hugo_test",all.files = T))
+})
+
+test_that("there is a directory", {
+  expect_true("./hugo_test" %in% list.dirs())
+})
+
