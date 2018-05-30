c6de83cf61ca4fc8e9d0cc553bb994f5fcbe6b35
mk <matkruit@gmail.com>
2018-05-30 12:40:42 +0200

    Prtotype of funcionality

--- R/hugo_share_object.R
+++ R/hugo_share_object.R
@@ -10,3 +10,12 @@
 hugo_share_object <- function(object) {
   # TODO implement share on github
 }
+
+
+# Korzysta z bilbioteki 'git2r'.
+# Przy pierwszym wywołaniu prosi użytkownika o podanie user/password
+# oraz o wybór, czy obiekt umieszcza w zdalnym repozytorium,
+# czy korzysta z lokalnej kopii do dodania obiektu.
+# Przy kolejnych, pyta, czy korzystać z wczytanych parametrów, czy podać nowe.
+# Zakładam, że korzystamy z repozytorium publicznego.
+# Pobranie obiektu polega na odpowiednio eleganckim (opakowanym w funckję, np hugo::hugo_load(object) przekazaniu parametrów koniecznych do wczytaniu pliku.
