c6d802cac3e337432c4f8a3c590f28591410180f
KasiaWo <32644095+KasiaWo@users.noreply.github.com>
2018-05-30 13:12:53 +0200

    Add files via upload

--- R/hugo_summarise_data.R
+++ R/hugo_summarise_data.R
@@ -1,14 +1,16 @@
-#' This function creates html or pdf report which summarise variables distribution. 
+#' This function creates html or pdf report which summarise variables distribution.
 #' This function uses package dataMaid to create  report.
-#' User can create list of paramters for function makeDataReport() 
+#' User can create list of paramters for function makeDataReport()
 #' and in next use this parmeters will be use in default.
 #'
 #'
-#' @param 
+#' @param
 #'
 #' @export
 #' @author Katarzyna Woznica
-#' 
-#'  hugo_summarise_data <- function(data, ...) {
-#'   
-#' }
+
+hugo_summarise_data <- function(data, ...){
+
+
+
+}
