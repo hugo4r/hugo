0f70decb50b65693ddbe0778844a034ec08e8529
komosinskid <32815122+komosinskid@users.noreply.github.com>
2018-05-30 12:20:57 +0200

    dk

--- R/hugo_read_data.R
+++ R/hugo_read_data.R
@@ -1,3 +1,12 @@
-# Author: Dariusz Komosinski
-
-# funkcja wczytuje dane
+#' Reads data to R in many ways
+#'
+#' @param path name of a directory where is file to read
+#'
+#' @export
+#' @author Dariusz Komosinski
+#' @examples
+#' hugo_read_data()
+#'
+hugo_read_data <- function(path){
+  return(0)
+}
