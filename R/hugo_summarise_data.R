3d385febcd77a6864a155015fd4a90d27c95fdc4
KasiaWo <32644095+KasiaWo@users.noreply.github.com>
2018-05-30 12:36:54 +0200

    Add files via upload

---
+++ R/hugo_summarise_data.R
@@ -0,0 +1,4 @@
+# This function creates html or pdf report which summarise variables distribution. 
+# This function uses package dataMaid to create  report.
+# User can create list of paramters for function makeDataReport() 
+# and in next use this parmeters will be use in default.
