42c904f821cbd3e2829a3297dc141988bf5e42c0
Anna Kozak <anna1993kozak@gmail.com>
2018-05-30 12:18:13 +0200

    update

---
+++ R/hugo_train_model.R
@@ -0,0 +1,34 @@
+#' Starts a New Hugo Investigation
+#'
+#'
+#' @export
+#' @author Anna Kozak
+#' @examples
+#' hugo_train_model()
+#'
+
+
+ 
+hugo_train_model <- function(data, formula){
+  # wyzaczenie zbioru treningowego - domyslnie boot
+  df.train <- trainControl(method = "boot", number = 10)
+  
+  ### pozostale metody: "boot", "boot632", "optimism_boot", "boot_all", "cv", "repeatedcv", "LOOCV", "LGOCV"
+  
+  
+  # model regresyjny
+  model.lm <- train(formula,  method="lm")
+  
+  # model drzew decyzyjnych
+  model.part <- train(formula, df.train, method = "rpart")
+  
+  # gbmlight
+  # pakiet lightgbm?
+  
+  
+  #ocena klasyfikatora - pakiet caret - trainControl(method="boot") - domyslnie
+  
+  
+  #otrzymany wynik
+  #mozemy zapytac uzytkownika czy chce zmienic metode oceny klasyfikatora
+}
