e8e55b2f78ccd07b5e7ceef0e18ce49b587a7fa6
elizakaczorek <32644047+elizakaczorek@users.noreply.github.com>
2018-05-30 12:50:14 +0200

    Update hugo_clean_data.R

--- R/hugo_clean_data.R
+++ R/hugo_clean_data.R
@@ -1,15 +1,14 @@
-# hugo_clean_data <- function(data, ...) {
-#   
-# }
+#' This function cleans data.
+#' Funkcja przyjmuje jako argument data.frame z danymi
+#' 1. imputuje braki danych
+#' 2. w zmiennych skośnych obcina wartości odstające do pewnego kwantyla
+#' 3. spłaszcza zmienne factorowe
+#' Funkcja zapisuje do katalogu data zbiór z wyczyszczonymi danymi i informuje o tym użytkownika.
+#'
+#' @param data Data that will be cleaned.
+#' @param quantile Kwantyl do jakiego obcina skośne zmienne numeryczne
+#' @param prop Frakcja występowania, poniżej której wartość zmiennej katogorycznej jest uznana za rzadką, i łączona z innymi rzadkimi w jeden factor 
+#'
+#' @author Eliza Kaczorek
+#' @export
 
-# Funkcja przyjmuje jako argument data.frame z danymi
-# 1. imputuje braki danych
-# 2. w zmiennych skośnych obcina wartości odstające do pewnego kwantyla
-# 3. spłaszcza zmienne factorowe
-
-# Dodatkowe argumenty:
-# - quantile - do jakiego kwantyla obciąć skośne zmienne numeryczne
-# - prop - frakcja występowania, poniżej której wartość zmiennej katogorycznej jest uznana za rzadką,
-#          i łączona z innymi rzadkimi w jeden factor 
-
-# Funkcja zapisuje do katalogu data zbiór z wyczyszczonymi danymi i informuje o tym użytkownika.
