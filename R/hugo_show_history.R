13fd41a50f433d1b4e5362720d5f7dfc8da221f2
lgelmo <32520474+lgelmo@users.noreply.github.com>
2018-05-30 12:36:31 +0200

    Add files via upload

---
+++ R/hugo_show_history.R
@@ -0,0 +1,18 @@
+#' Showes all the comends which have been used
+#'
+#' 
+#'
+#' @export
+#' @author Maciej Kurek
+#' @examples
+#' hugo_show_history()
+
+
+hugo_show_history<-function(){
+  
+  
+}
+
+
+
+
