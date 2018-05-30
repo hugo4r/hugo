79b0f9a6b1c441e540497cbaac63535a85c6f8f3
Małgosia <32644089+MalgosiaL@users.noreply.github.com>
2018-05-30 13:04:49 +0200

    funkcja

---
+++ R/hugo_memorise_model.R
@@ -0,0 +1,11 @@
+#' Saves an object as: .rda file, .md file and a markdown table as xlsx and docx  
+#' 
+#' @export
+#' @author Ma³gorzata £azêcka
+#' @example 
+#' hugo_memorise_model()
+#' 
+
+hugo_memorise_model <- function(model){
+  
+}
