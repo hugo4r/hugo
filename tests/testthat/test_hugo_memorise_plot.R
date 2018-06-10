99b6eded0c1653ab9e038a6a99f0f5205d4153a2
Joanna Golawska <32520607+golawskaj@users.noreply.github.com>
2018-06-10 14:40:04 +0200

    update tests

--- tests/testthat/test_hugo_memorise_plot.R
+++ tests/testthat/test_hugo_memorise_plot.R
@@ -27,16 +27,13 @@ test_that("function without returning value", {
   expect_null(hugo_memorise_plot(plot(1:10)))
 })
 
-#test_that("plot created with package ggplot2 is correctly saved with name in input", {
-#  df <- data.frame(gp = factor(rep(letters[1:3], each = 10)),y = rnorm(30))
-#  require(ggplot2)
-#  plot_gg <- ggplot(df, aes(gp, y)) + geom_point()
-#  expect_output(hugo_memorise_plot(plot_gg, "test_name"))
-#})
-#
-#
-#
-#
+test_that("plot created with package ggplot2 is correctly saved with name in input", {
+  df <- data.frame(gp = factor(rep(letters[1:3], each = 10)),y = rnorm(30))
+  require(ggplot2)
+  plot_gg <- ggplot(df, aes(gp, y)) + geom_point()
+  expect_output(hugo_memorise_plot(plot_gg, "test_name"))
+})
+
 #test_that("plot created with package ggplot2 in function is correctly saved", {
 #  df <- data.frame(gp = factor(rep(letters[1:3], each = 10)),y = rnorm(30))
 #  expect_output(hugo_memorise_plot(ggplot(df, aes(gp, y)) + geom_point(), "test_name2"))
@@ -67,12 +64,12 @@ test_that("object without name is saved",{
   expect_false(length(list.files("./hugo_test/gallery", pattern = "(plot)")) == 0)
 })
 
-#test_that("object without name on input are saved with next numbers",{
-#  hugo_memorise_plot(plot(1:5))
-#  hugo_memorise_plot(plot(1:10, type = "l"))
-#  numb_of_plots <- substr(list.files("./hugo_test/gallery", pattern = "(plot)"), start = 5L, stop = 6L)
-#  numb_of_plots <- unique(as.numeric(numb_of_plots)[!is.na(as.numeric(numb_of_plots))])[order(unique(as.numeric(numb_of_plots)[!is.na(as.numeric(numb_of_plots))]), decreasing = TRUE)]
-#  expect_true(numb_of_plots[1] == numb_of_plots[2] + 1)
-#})
-#
+test_that("object without name on input are saved with next numbers",{
+  hugo_memorise_plot(plot(1:5))
+  hugo_memorise_plot(plot(1:10, type = "l"))
+  numb_of_plots <- substr(list.files("./hugo_test/gallery", pattern = "(plot)"), start = 5L, stop = 6L)
+  numb_of_plots <- unique(as.numeric(numb_of_plots)[!is.na(as.numeric(numb_of_plots))])[order(unique(as.numeric(numb_of_plots)[!is.na(as.numeric(numb_of_plots))]), decreasing = TRUE)]
+  expect_true(numb_of_plots[1] == numb_of_plots[2] + 1)
+})
+
 unlink("hugo_test", recursive = TRUE)
