966d065676192b62a32ead82a791b19068635acb
elizakaczorek <32644047+elizakaczorek@users.noreply.github.com>
2018-06-11 22:58:30 +0200

    update test_hugo_clean_data.R

--- tests/testthat/test_hugo_clean_data.R
+++ tests/testthat/test_hugo_clean_data.R
@@ -10,6 +10,22 @@ test_that("Argument 'data' is not a data.frame",{
   expect_error(hugo_clean_data(matrix(0,nrow = 3, ncol = 3)))
 })
 
-test_that("There are still NA's in data after cleaning", {
+test_that("There should not be NA's in data after cleaning", {
   expect_equal(sum(is.na(hugo_clean_data(airquality))), 0)
+})
+
+test_that("Filling NA's for data with factors (one mode)", {
+  expect_equal({
+    data <- Puromycin
+    data[12, "state"] <- NA
+    sum(is.na(hugo_clean_data(data)))
+    }, 0)
+})
+
+test_that("Filling NA's for data with factors (two modes)", {
+  expect_equal({
+    data <- Puromycin
+    data[12:15, "state"] <- NA
+    sum(is.na(hugo_clean_data(data)))
+  }, 0)
 })
