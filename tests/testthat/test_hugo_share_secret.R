4595173bdc71f694715b88be563ed2f69efbd3d9
Kamil Romaszko <kamil.romaszko@gmail.com>
2018-06-11 23:07:21 +0200

    Complete hugo_share_secret + tests
    + pkgdown site rebuild

--- tests/testthat/test_hugo_share_secret.R
+++ tests/testthat/test_hugo_share_secret.R
@@ -1,4 +1,4 @@
-context("Check hugo_share_secret() function")
+context("Check hugo_share_secret and hugo_get_secret functions")
 
 test_that("object is encrypted and decrypted", {
   encrypted_object <- encrypt_object(iris, "pass")
@@ -11,17 +11,79 @@ test_that("object is not decrypted with wrong password", {
   expect_error(decrypt_object(encrypted_object, "wrong_pass"))
 })
 
-test_that("object is encrypted and shared", {
-  # TODO test encrypted share when it's ready
-  expect_silent(hugo_share_secret(iris, "pass"))
-})
-
-requireNamespace_mock <- function(package, quietly) {
-  return(false)
+hugo_share_object_success_mock <- function(object) {
+  decrypted_iris <- decrypt_object(object, "pass")
+  expect_equal(decrypted_iris, iris)
+  return("hugo_get_object('user', 'repo', 'object_123')")
 }
 
-test_that("encryption is not used when there is no \"sodium\" package", {
+hugo_share_object_fail_mock <- function(object) {
+  decrypted_iris <- decrypt_object(object, "pass")
+  expect_equal(decrypted_iris, iris)
+  return(NULL)
+}
+
+test_that("object is encrypted and shared", {
+  with_mock(hugo_share_object = hugo_share_object_success_mock,
+            {
+              get_string <- hugo_share_secret(iris, "pass")
+              expect_equal(get_string,
+                           "hugo_get_secret('user', 'repo', 'object_123', 'pass')")
+            })
+})
+
+test_that("should return NULL when there was error", {
+  with_mock(hugo_share_object = hugo_share_object_fail_mock,
+            {
+              get_string <- hugo_share_secret(iris, "pass")
+              expect_null(get_string)
+            })
+})
+
+hugo_get_object_success_mock <- function(user, repo_name, object_name) {
+  expect_equal(user, "user")
+  expect_equal(repo_name, "repo")
+  expect_equal(object_name, "iris_secret")
+  return(encrypt_object(iris, "pass"))
+}
+
+hugo_get_object_fail_mock <- function(user, repo_name, object_name) {
+  expect_equal(user, "user")
+  expect_equal(repo_name, "repo")
+  expect_equal(object_name, "iris_secret")
+  return(NULL)
+}
+
+test_that("object is fetched and decrypted", {
+  with_mock(hugo_get_object = hugo_get_object_success_mock,
+            {
+              decrypted_object <- hugo_get_secret("user", "repo", "iris_secret", "pass")
+              expect_equal(decrypted_object, iris)
+            })
+})
+
+test_that("get return NULL when there was an error during fetch", {
+  with_mock(hugo_get_object = hugo_get_object_fail_mock,
+            {
+              object <- hugo_get_secret("user", "repo", "iris_secret", "pass")
+              expect_null(object)
+            })
+})
+
+
+
+requireNamespace_mock <- function(package, quietly) {
+  return(FALSE)
+}
+
+test_that("function fails when there is no \"sodium\" package", {
   with_mock(requireNamespace = requireNamespace_mock,
     expect_error(hugo_share_secret(iris, "pass"))
+  )
+})
+
+test_that("function fails when there is no \"sodium\" package", {
+  with_mock(requireNamespace = requireNamespace_mock,
+            expect_error(hugo_get_secret('user', 'repo', 'object_123', "pass"))
   )
 })
