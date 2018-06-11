8ca4a9e598eea27c1ae32b13285eec7603e6265c
lgelmo <32520474+lgelmo@users.noreply.github.com>
2018-06-11 15:19:08 +0200

    Update hugo_show_history.R

--- R/hugo_show_history.R
+++ R/hugo_show_history.R
@@ -20,9 +20,9 @@ hugo_show_history<-function(){
 
 add_to_history<-function(function_name){
   
-  savehistory("histor.txt")
-  hist<-readLines("histor.txt")
-  file.remove("histor.txt")
+   savehistory()
+   hist<-readLines(".Rhistory")
+   file.remove(".Rhistory")
   
   pat<-paste(function_name,"\\(",sep="")
   index<-which(grepl(pat,hist[]))
