1d3d8a76d6e4c61e97a0e036cf90178c26469f0b
Anna Gierlak <32520597+AnnaGierlak@users.noreply.github.com>
2018-06-12 12:15:35 +0200

    Add files via upload

--- tests/testthat/test_hugo_memorise.R
+++ tests/testthat/test_hugo_memorise.R
@@ -1,39 +1,49 @@
 context('Check hugo_memorise() function')
 
+
+test_that('hugo_start_investigation() has been executed', {
+  test1 <- 1:3
+  expect_error(hugo_memorise(test1), 'Call hugo_start_investigation\\(\\) for starting the new investigation.')
+})
+
 hugo_start_investigation('hugo_test')
 
-
-test_that('the function takes argument', {
+test_that('the function takes an argument', {
   expect_error(hugo_memorise(), 'There is no object to be saved.')
 })
 
 test_that('the function takes exactly one argument', {
-  a <- 1:3
-  b <- 4:6
-  expect_error(hugo_memorise(a,b))
+  test2 <- 1:3
+  test3 <- 4:6
+  expect_error(hugo_memorise(test2,test3))
 })
 
-test_that('an object is saved without warnings/errors', {
-  a1 <- 1:3
-  expect_output(hugo_memorise(a1), cat('Copy of the \"a1\" object is stored in hugo_memorise_test/memory/.\n'))
+test_that('an object is saved without any warnings/errors', {
+  test4 <- list('abcdef', seq(0,5,0.1), c(TRUE,FALSE))
+  expect_output(hugo_memorise(test4), cat('Copy of the \"test4\" object is stored in hugo_memorise_test/memory/.\n'))
 })
 
 test_that('there is a memory subdirectory after saving an object', {
-  a2 <- 1:3
-  hugo_memorise(a2)
+  test5 <- 1:3
+  hugo_memorise(test5)
   expect_true('memory' %in% list.files('./hugo_test'))
 })
 
 test_that('an object is saved correctly', {
-  a3 <- 1:3
-  hugo_memorise(a3)
-  expect_true('a3.rda' %in% list.files('./hugo_test/memory'))
+  test6 <- 1:3
+  hugo_memorise(test6)
+  expect_true('test6.rda' %in% list.files('./hugo_test/memory'))
 })
 
+test_that('overwriting an object', {
+  test7 <- 1:3
+  hugo_memorise(test7)
+  test7 <- 4:6
+  expect_output(hugo_memorise(test7))
+})
+
+
 unlink('hugo_test', recursive = TRUE)
-
-
-
 
 
 
