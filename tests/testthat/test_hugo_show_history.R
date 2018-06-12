789b69ca54c2323f2cfab103d125e2225f9c57f7
lgelmo <32520474+lgelmo@users.noreply.github.com>
2018-06-12 15:40:10 +0200

    Update test_hugo_show_history.R

--- tests/testthat/test_hugo_show_history.R
+++ tests/testthat/test_hugo_show_history.R
@@ -1,7 +1,28 @@
 context("Check hugo_show_history() function")
 
-test_that("error when using any arguments", {
+test_that("error when using wrong arguements", {
   expect_error(hugo_show_history("aaa"))
   expect_error(hugo_show_history(c(1,2)))
-  expect_error(hugo_show_history("aaa",12,list("b",1)))
+  expect_error(hugo_show_history("aaa",12,c(1,1)))
+  expect_error(hugo_show_history(1.5))
+  expect_error(hugo_show_history(sqrt(7)))
+})
+
+test_that("error when using too many arguements", {
+  expect_error(hugo_show_history(1,2,3))
+  expect_error(hugo_show_history(first=1,specyfic=c(2,7)))
+  expect_error(hugo_show_history(last=1,specyfic=c(2,7)))
+})
+
+test_that("success when expected to success", {
+  expect_failure(expect_error(hugo_show_history(5)))
+  expect_failure(expect_error(hugo_show_history(first=10)))
+  expect_failure(expect_error(hugo_show_history(specyfic=c(-10))))
+  expect_failure(expect_error(hugo_show_history(specyfic=c(1,5,10))))
+})
+
+test_that("there are correct entries in history", {
+  expect_error(hugo_show_history(5,4))
+  expect_equal(.hugoEnv$history[length(.hugoEnv$history)],"hugo_show_history(last = 5, first = 4)")
+ 
 })
