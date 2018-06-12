8328e27fd042ef1428839ee99c157a98518494de
Anna Kozak <anna1993kozak@gmail.com>
2018-06-12 12:35:09 +0200

    update hugo_train_model

--- R/hugo_train_model.R
+++ R/hugo_train_model.R
@@ -13,18 +13,112 @@
 #' }
 #' 
 
-
 hugo_train_model <- function(data, formula) {
   .hugoEnv$history[length(.hugoEnv$history) + 1] <-
     deparse(match.call())
+  
+  #make a trainControl set
+  data_division <- function(data) {
+    control <-
+      trainControl(method = "repeatedcv",
+                   number = 10,
+                   repeats = 3)
+    return(control)
+  }
+  
+  #regresion model
+  glm_model <- function(control, data, formula) {
+    return(train(
+      formula,
+      data = data,
+      method = "glm",
+      trControl = control
+    ))
+  }
+  #random forest model
+  randomforest_model <- function(control, data, formula) {
+    return(train(
+      formula,
+      data = data,
+      method = "rf",
+      trControl = control
+    ))
+  }
+  #gbm model
+  gbm_model <- function(control, data, formula) {
+    return(train(
+      formula,
+      data = data,
+      method = "gbm",
+      trControl = control
+    ))
+  }
+  #results
+  model_results <- function(model_1, model_2, model_3) {
+    outcome <-
+      resamples(list(
+        model_glm = model_1,
+        model_randomforest = model_2,
+        model_gbm = model_3
+      ))
+    sum_results <<- summary(outcome)
+    cat("Statistical variability measured by Accuracy.\n")
+    cat(paste(
+      "The best model is ",
+      sub(".*_", "", names(
+        which.max(sum_results$statistics$Accuracy[, 4])
+      )),
+      ".",
+      " Accuracy = ",
+      round(max(sum_results$statistics$Accuracy[, 4]), 2),
+      "\n",
+      sep = ""
+    ))
+    return(names(which.max(sum_results$statistics$Accuracy[, 4])))
+  }
+  #important variable in model
+  important_variable <- function(model) {
+    value <- varImp(get(model))$importance
+    number_of_variable <-
+      menu(c("Yes", "No"), title = "Hugo shows you the 20 first variables. Do you want see all?\n")
+    if (number_of_variable == "No") {
+      n <- 20
+    } else{
+      n <- nrow(value)
+    }
+    if (model != "model_gbm") {
+      row.names(value)[order(-value)]
+      varImp(get(model))$importance$Overall[order(-value$Overall)]
+      cat(paste(row.names(value)[order(-value$Overall)][1:n],
+                paste(
+                  "(", round(value$Overall[order(-value$Overall)][1:n], 2), ")", sep = ""
+                )), sep = ", ")
+    } else{
+      cat(paste(
+        summary(get(model))$var[1:n],
+        " ",
+        "(",
+        round(summary(get(model))$rel.inf[1:n], 2),
+        ")",
+        ",",
+        sep = ""
+      ))
+    }
+  }
+  
+  
   cat("Hugo starts training models.\n")
   formula <- stats::formula(formula)
+  if (length(levels(data[, c(as.character(formula[[2]]))])) != 2) {
+    return(
+      cat(
+        "Response variable has more than two classes. Wrong data to binary classification.\n"
+      )
+    )
+  }
   if (!is.factor(data[, c(as.character(formula[[2]]))])) {
     data[, c(as.character(formula[[2]]))] <-
       as.factor(data[, c(as.character(formula[[2]]))])
-  }
-  if (length(levels(data[, c(as.character(formula[[2]]))])) != 2) {
-    return(cat("Response variable has more than two classes. Wrong data to binary classification.\n"))
   }
   control <- data_division(data)
   model_glm <<- glm_model(control, data, formula)
@@ -37,92 +131,3 @@ hugo_train_model <- function(data, formula) {
   important_variable(result)
   return(get(result))
 }
-#make a trainControl set
-data_division <- function(data) {
-  control <-
-    trainControl(method = "repeatedcv",
-                 number = 10,
-                 repeats = 3)
-  return(control)
-}
-
-#regresion model
-glm_model <- function(control, data, formula) {
-  return(train(
-    formula,
-    data = data,
-    method = "glm",
-    trControl = control
-  ))
-}
-#random forest model
-randomforest_model <- function(control, data, formula) {
-  return(train(
-    formula,
-    data = data,
-    method = "rf",
-    trControl = control
-  ))
-}
-#gbm model
-gbm_model <- function(control, data, formula) {
-  return(train(
-    formula,
-    data = data,
-    method = "gbm",
-    trControl = control
-  ))
-}
-#results
-model_results <- function(model_1, model_2, model_3) {
-  outcome <-
-    resamples(list(
-      model_glm = model_1,
-      model_randomforest = model_2,
-      model_gbm = model_3
-    ))
-  sum_results <<- summary(outcome)
-  cat("Statistical variability measured by Accuracy.\n")
-  cat(paste(
-    "The best model is ",
-    sub(".*_", "", names(
-      which.max(sum_results$statistics$Accuracy[, 4])
-    )),
-    ".",
-    " Accuracy = ",
-    round(max(sum_results$statistics$Accuracy[, 4]), 2),
-    "\n",
-    sep = ""
-  ))
-  return(names(which.max(sum_results$statistics$Accuracy[, 4])))
-}
-#important variable in model
-important_variable <- function(model) {
-  value <- varImp(get(model))$importance
-  number_of_variable <-
-    menu(c("Yes", "No"), title = "Hugo shows you the 20 first variables. Do you want see all?\n")
-  if (number_of_variable == "No") {
-    n <- 20
-  } else{
-    n <- nrow(value)
-  }
-  if (model != "model_gbm") {
-    row.names(value)[order(-value)]
-    varImp(get(model))$importance$Overall[order(-value$Overall)]
-    cat(paste(row.names(value)[order(-value$Overall)][1:n],
-              paste(
-                "(", round(value$Overall[order(-value$Overall)][1:n], 2), ")", sep = ""
-              )), sep = ", ")
-  } else{
-    cat(paste(
-      summary(get(model))$var[1:n],
-      " ",
-      "(",
-      round(summary(get(model))$rel.inf[1:n], 2),
-      ")",
-      ",",
-      sep = ""
-    ))
-  }
-}
-
