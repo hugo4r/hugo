3d42677125745abfa27704c39dcd0b6d8519d0b6
KasiaWo <32644095+KasiaWo@users.noreply.github.com>
2018-06-13 06:59:33 +0200

    update tests

--- tests/testthat/test_hugo_summarise_data.R
+++ tests/testthat/test_hugo_summarise_data.R
@@ -9,20 +9,36 @@ test_that('successfully generating report',{
   options(hugo.connection_in = f)
   options(hugo.connection_out = g)
 
-  ans <- paste(c('pdf','TRUE','FALSE','all', 'TRUE', '2', 'iris', 'iris',
-                 'pdf','TRUE','FALSE','all', 'TRUE', '2', 'iris', 'iris',
+  ans <- paste(c( '3', 'iris', 'iris',
+                  '2', 'iris', 'iris',
                  'iris', 'iris',
-                 'pdf','TRUE','FALSE','all', 'TRUE', '2', 'cars', 'cars'),collapse = '\n')
+                  '2', 'cars', 'cars'),collapse = '\n')
   write(ans,f)
   hugo_start_investigation("hugo_test")
-  expect_output(hugo_summarise_data(iris, FALSE), "Success!")
-  expect_output(hugo_summarise_data(iris), "Success!")
+  expect_output(with_mock(
+    "hugo:::get_output_from_user" =function(){'pdf'},
+    "hugo:::get_replace_from_user" =function(){TRUE},
+    "hugo:::get_open_from_user" =function(){FALSE},
+    "hugo:::get_smart_factor_from_user" =function(){TRUE},
+    hugo_summarise_data(iris, FALSE)), "Success!")
+
+  expect_output(with_mock(
+    "hugo:::get_output_from_user" =function(){'pdf'},
+    "hugo:::get_replace_from_user" =function(){TRUE},
+    "hugo:::get_open_from_user" =function(){FALSE},
+    "hugo:::get_smart_factor_from_user" =function(){TRUE},
+    hugo_summarise_data(iris)), "Success!")
+
   expect_output(with_mock(
     "hugo:::menu_prev_settings" = function(default_settings){return(TRUE)},
     hugo_summarise_data(iris)), "Success!")
 
   expect_output(with_mock(
     "hugo:::menu_prev_settings" = function(default_settings){return(FALSE)},
+    "hugo:::get_output_from_user" =function(){'html'},
+    "hugo:::get_replace_from_user" =function(){TRUE},
+    "hugo:::get_open_from_user" =function(){FALSE},
+    "hugo:::get_smart_factor_from_user" =function(){TRUE},
     hugo_summarise_data(cars)), "Success!")
 
   options(hugo.connection_in = stdin())
@@ -40,13 +56,24 @@ test_that('wrong type of data',{
   g <- file()
   options(hugo.connection_in = f)
   options(hugo.connection_out = g)
-
-  ans <- paste(c(c('pdf','TRUE','FALSE','all', 'TRUE', '2', 'iris', 'iris',
-                   'iris', 'iris')),collapse = '\n')
+  ans <- paste(c( '3', 'iris', 'iris',
+                  '2', 'iris', 'iris'),collapse = '\n')
   write(ans,f)
-
   hugo_start_investigation("hugo_test")
-  expect_error(hugo_summarise_data("dane"))
+  expect_error(with_mock(
+    "hugo:::get_output_from_user" =function(){'pdf'},
+    "hugo:::get_replace_from_user" =function(){TRUE},
+    "hugo:::get_open_from_user" =function(){FALSE},
+    "hugo:::get_smart_factor_from_user" =function(){TRUE},
+    hugo_summarise_data("dane", FALSE)))
+
+  expect_error(with_mock(
+    "hugo:::get_output_from_user" =function(){'html'},
+    "hugo:::get_replace_from_user" =function(){TRUE},
+    "hugo:::get_open_from_user" =function(){FALSE},
+    "hugo:::get_smart_factor_from_user" =function(){TRUE},
+    hugo_summarise_data(25)))
+
 
 
 
@@ -68,7 +95,7 @@ test_that('wrong type of overwrite parameters',{
 })
 
 
-test_that('wrong type of typed parameters- output',{
+test_that('wrong type of choosen parameters',{
   tmp <- getOption("hugo.know_summary_parameters")
   options(hugo.know_summary_parameters = F)
   options(hugo.use_summary_parameters = T)
@@ -77,34 +104,125 @@ test_that('wrong type of typed parameters- output',{
   g <- file()
   options(hugo.connection_in = f)
   options(hugo.connection_out = g)
-  ans <- paste(c(
-                'word','TRUE','FALSE','all', 'TRUE', '2', 'iris', 'iris',
-                 'html','null','FALSE','all', 'TRUE', '2', 'cars', 'cars',
-                 'html','TRUE','false','all', 'TRUE', '2', 'cars', 'cars',
-                 'html','TRUE','FALSE','all', 't', '2', 'cars', 'cars',
-                 'html','TRUE','FALSE','all', 'TRUE', 'null', 'cars', 'cars'),collapse = '\n')
+  ans <- paste(c( '3', 'iris', 'iris',
+                  '2', 'cars', 'cars',
+                  '2', 'cars', 'cars',
+                  '2', 'iris', 'iris'),collapse = '\n')
   write(ans,f)
-
-  expect_message(hugo_summarise_data(data = iris), "Incorrect format of output. Default html will be used.")
+  hugo_start_investigation("hugo_test")
   expect_message(with_mock(
-    "hugo:::menu_prev_settings" = function(default_settings){return(FALSE)},
-    hugo_summarise_data(data = cars))
-  , "Incorrect value. Default TRUE will be used.")
-
-  expect_message(with_mock(
-    "hugo:::menu_prev_settings" = function(default_settings){return(FALSE)},
-    hugo_summarise_data(data = cars))
-    , "Incorrect value. Default TRUE will be used.")
+    "hugo:::get_output_from_user" =function(){NULL},
+    "hugo:::get_replace_from_user" =function(){TRUE},
+    "hugo:::get_open_from_user" =function(){FALSE},
+    "hugo:::get_smart_factor_from_user" =function(){TRUE},
+    hugo_summarise_data(iris)), "Default value will be used: html")
 
   expect_message(with_mock(
     "hugo:::menu_prev_settings" = function(default_settings){return(FALSE)},
-    hugo_summarise_data(data = cars))
-    , "Incorrect value. Default TRUE will be used.")
+    "hugo:::get_output_from_user" =function(){'html'},
+    "hugo:::get_replace_from_user" =function(){NULL},
+    "hugo:::get_open_from_user" =function(){FALSE},
+    "hugo:::get_smart_factor_from_user" =function(){TRUE},
+    hugo_summarise_data(cars)), "Default value will be used: TRUE")
 
   expect_message(with_mock(
     "hugo:::menu_prev_settings" = function(default_settings){return(FALSE)},
-    hugo_summarise_data(data = cars))
-    , "Incorrect value. Default 2 will be used.")
+    "hugo:::get_output_from_user" =function(){'html'},
+    "hugo:::get_replace_from_user" =function(){TRUE},
+    "hugo:::get_open_from_user" =function(){NULL},
+    "hugo:::get_smart_factor_from_user" =function(){TRUE},
+    hugo_summarise_data(cars)), "Default value will be used: FALSE")
+
+  expect_message(with_mock(
+    "hugo:::menu_prev_settings" = function(default_settings){return(FALSE)},
+    "hugo:::get_output_from_user" =function(){'html'},
+    "hugo:::get_replace_from_user" =function(){TRUE},
+    "hugo:::get_open_from_user" =function(){FALSE},
+    "hugo:::get_smart_factor_from_user" =function(){NULL},
+    hugo_summarise_data(iris)), "Default value will be used: TRUE")
+
+
+  options(hugo.connection_in = stdin())
+  options(hugo.connection_out = stdout())
+  options(hugo.know_summary_parameters = tmp)
+  close(f)
+  close(g)
+})
+
+test_that('wrong type of typed parameters',{
+  tmp <- getOption("hugo.know_summary_parameters")
+  options(hugo.know_summary_parameters = F)
+  options(hugo.use_summary_parameters = T)
+  options(hugo.use_default_name_summary_parameters = T)
+  f <- file()
+  g <- file()
+  options(hugo.connection_in = f)
+  options(hugo.connection_out = g)
+  ans <- paste(c( '30', 'iris', 'iris',
+                  'dwa', 'cars', 'cars',
+                  '2.5', 'cars', 'cars',
+                  '-2', 'iris', 'iris',
+                  '2', '', 'iris',
+                  '2', 'NULL', 'iris',
+                  '2', 'cars', '',
+                  '3', 'cars', 'NULL'
+                  ),collapse = '\n')
+  write(ans,f)
+  hugo_start_investigation("hugo_test")
+  expect_message(with_mock(
+    "hugo:::get_output_from_user" =function(){'html'},
+    "hugo:::get_replace_from_user" =function(){TRUE},
+    "hugo:::get_open_from_user" =function(){FALSE},
+    "hugo:::get_smart_factor_from_user" =function(){TRUE},
+    hugo_summarise_data(iris)), "Incorrect value. Default 2 will be used.")
+
+  expect_message(with_mock(
+    "hugo:::menu_prev_settings" = function(default_settings){return(FALSE)},
+    "hugo:::get_output_from_user" =function(){'html'},
+    "hugo:::get_replace_from_user" =function(){TRUE},
+    "hugo:::get_open_from_user" =function(){FALSE},
+    "hugo:::get_smart_factor_from_user" =function(){TRUE},
+    hugo_summarise_data(cars)), "Incorrect value. Default 2 will be used.")
+
+  expect_message(with_mock(
+    "hugo:::menu_prev_settings" = function(default_settings){return(FALSE)},
+    "hugo:::get_output_from_user" =function(){'html'},
+    "hugo:::get_replace_from_user" =function(){TRUE},
+    "hugo:::get_open_from_user" =function(){FALSE},
+    "hugo:::get_smart_factor_from_user" =function(){TRUE},
+    hugo_summarise_data(cars)), "Incorrect value. Default 2 will be used.")
+
+  expect_message(with_mock(
+    "hugo:::menu_prev_settings" = function(default_settings){return(FALSE)},
+    "hugo:::get_output_from_user" =function(){'html'},
+    "hugo:::get_replace_from_user" =function(){TRUE},
+    "hugo:::get_open_from_user" =function(){FALSE},
+    "hugo:::get_smart_factor_from_user" =function(){TRUE},
+    hugo_summarise_data(cars)), "Incorrect value. Default 2 will be used.")
+
+  expect_message(with_mock(
+    "hugo:::menu_prev_settings" = function(default_settings){return(FALSE)},
+    "hugo:::get_output_from_user" =function(){'html'},
+    "hugo:::get_replace_from_user" =function(){TRUE},
+    "hugo:::get_open_from_user" =function(){FALSE},
+    "hugo:::get_smart_factor_from_user" =function(){TRUE},
+    hugo_summarise_data(cars)), "Default filename will be used")
+
+  expect_message(with_mock(
+    "hugo:::menu_prev_settings" = function(default_settings){return(FALSE)},
+    "hugo:::get_output_from_user" =function(){'html'},
+    "hugo:::get_replace_from_user" =function(){TRUE},
+    "hugo:::get_open_from_user" =function(){FALSE},
+    "hugo:::get_smart_factor_from_user" =function(){TRUE},
+    hugo_summarise_data(cars)), "Default filename will be used")
+
+  expect_message(with_mock(
+    "hugo:::menu_prev_settings" = function(default_settings){return(FALSE)},
+    "hugo:::get_output_from_user" =function(){'html'},
+    "hugo:::get_replace_from_user" =function(){TRUE},
+    "hugo:::get_open_from_user" =function(){FALSE},
+    "hugo:::get_smart_factor_from_user" =function(){TRUE},
+    hugo_summarise_data(cars)), "Default title will be used")
 
 
   options(hugo.connection_in = stdin())
@@ -117,13 +235,22 @@ test_that('wrong type of typed parameters- output',{
 
 
 
+requireNamespace_mock <- function(package, quietly) {
+  return(false)
+}
+
+test_that("throws error when there is no \"dataMaid\" package", {
+  with_mock(requireNamespace = requireNamespace_mock,
+            expect_error(hugo_summarise_data(iris))
+  )
+})
 
 
 requireNamespace_mock <- function(package, quietly) {
   return(false)
 }
 
-test_that("throws error when there is no \"dataMaid\" package", {
+test_that("throws error when there is no \"rmarkdown\" package", {
   with_mock(requireNamespace = requireNamespace_mock,
             expect_error(hugo_summarise_data(iris))
   )
