df9ddae929af7233c518fb09ea12f8c751f4bb55
Mateusz Kobyłka <32520662+kobylkam@users.noreply.github.com>
2018-06-16 13:08:50 +0200

    Update tests

--- tests/testthat/test_hugo_continue_investigation.R
+++ tests/testthat/test_hugo_continue_investigation.R
@@ -10,84 +10,67 @@ test_that("No saved sessions exist",{
 
 test_that("Variables are loaded",{
 
-  e <- new.env()
-  e$a <- 5
-  hugo_save_investigation(session_name = "test_session",envir = e)
-  remove(a, envir=e)
-  hugo_continue_investigation( session_name = "test_session", envir = e)
-  expect_equal(e$a,5)
+  test_environment <- new.env()
+  test_environment$test_variable <- 5
+  hugo_save_investigation(session_name = "test_session",envir = test_environment)
+  remove(test_variable, envir=test_environment)
+  hugo_continue_investigation( session_name = "test_session", envir = test_environment)
+  expect_equal(test_environment$test_variable,5)
 
 })
 
 test_that("Single session is loaded",{
 
-  e <- new.env()
-  expect_error(hugo_continue_investigation(envir=e),NA) #Expect success
+  test_environment <- new.env()
+  expect_error(hugo_continue_investigation(envir=test_environment),NA) #Expect success
 
 })
 
 
 test_that("Specified session is loaded",{
 
-  e <- new.env()
-  e$b <- 5
-  hugo_save_investigation(session_name="test_session 2",envir = e)
-  remove(b,envir=e)
-  hugo_continue_investigation(session_name = "test_session 2",envir = e)
-  expect_equal(e$b,5)
+  test_environment <- new.env()
+  test_environment$test_variable <- 5
+  hugo_save_investigation(session_name="test_session 2",envir = test_environment)
+  remove(test_variable,envir=test_environment)
+  hugo_continue_investigation(session_name = "test_session 2",envir = test_environment)
+  expect_equal(test_environment$test_variable,5)
 
 })
 
 
 test_that("Multiple sessions handling",{
 
-  e <- new.env()
-  e$b <- 5
-  hugo_save_investigation(session_name="test_session 3",envir = e)
-  remove(b,envir = e)
-  f <- file()
-  options(hugo.connection_in = f)
-  ans <- "test_session 3"
-  write(ans,f)
-  hugo_continue_investigation(envir=e)
+  test_environment <- new.env()
+  test_environment$test_variable <- 5
+  hugo_save_investigation(session_name="test_session 3",envir = test_environment)
+  remove(test_variable,envir = test_environment)
+  test_connection <- file()
+  options(hugo.connection_in = test_connection)
+  answer <- "test_session 3"
+  write(answer,test_connection)
+  hugo_continue_investigation(envir=test_environment)
   options(hugo.connection_in = stdin())
-  close(f)
-  expect_equal(e$b,5)
+  close(test_connection)
+  expect_equal(test_environment$test_variable,5)
 
 })
 
 test_that("Invalid session name is provided",{
 
-  e <- new.env()
-  expect_error(hugo_continue_investigation(session_name="Invalid",envir=e))
+  test_environment <- new.env()
+  expect_error(hugo_continue_investigation(session_name="Invalid",envir=test_environment))
 })
 
 test_that("Missing package is loaded",{
 
-  e <- new.env()
+  test_environment <- new.env()
   library(datasets)
-  hugo_save_investigation(session_name="test_session 4",envir=e)
+  hugo_save_investigation(session_name="test_session 4",envir=test_environment)
   detach(name="package:datasets", unload=TRUE, force = TRUE)
-  hugo_continue_investigation(session_name="test_session 4",envir=e)
+  hugo_continue_investigation(session_name="test_session 4",envir=test_environment)
   expect_true("datasets" %in% .packages())
 })
-
-#test_that("Missing package is installed",{
-#
-# e <- new.env()
-#    install.packages("astsa")
-#    library(astsa)
-#  }
-#  hugo_save_investigation(session_name = "test_session 5",envir=e)
-#  detach(name="package:astsa", unload=TRUE, force = TRUE)
-#  remove.packages("astsa")
-#  expect_error(hugo_continue_investigation(path="hugo_test_continue",session_name="test_session 5",envir=e))
-#  expect_true("astsa" %in% .packages())
-#
-#})
-
-
-
 test_that("Missing package cannot be installed",{
 
   e <- new.env()
